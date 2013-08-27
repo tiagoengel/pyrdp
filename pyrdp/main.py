931fc8f3610bb4afb61f2db3638eb1504b3a3786
tiago <tiagohngl@gmail.com>
2013-08-26 22:11:14 -0300

    QtSingleApplication and signals

--- pyrdp/main.py
+++ pyrdp/main.py
@@ -3,42 +3,27 @@ Created on 20/08/2013
 
 @author: tiago
 '''
-from PyQt4 import QtGui
-from pyrdp.rdp import Settings, RdpConnection
-from pyrdp.ui import MainWindow
-import socket
+
+from pyrdp.ui import QtSingleApplication, MainWindow
 import sys
 
 if __name__ == '__main__':
-#     app = QtGui.QApplication(sys.argv)
-#     screen_rect = app.desktop().screenGeometry()
-#     width, height = screen_rect.width(), screen_rect.height()
-#     
-#     main_window = MainWindow(width * 0.95, height * 0.90)
-#     main_window.show()
-#     
-#     sett = Settings()
-#     sett.u = 'tiago.h'
-#     sett.p = 'tiagost1'    
-#     sett.v = 'intersys.com.br'
-#     
-#     conn = RdpConnection(sett)
-#         
-#     main_window.add_tab(conn)
-#     
-#     app.exec_()
-#     
-#     main_window.close_all()
-    HOST, PORT = "localhost", 9999
-    data = " ".join(sys.argv[1:])
     
-    # Create a socket (SOCK_STREAM means a TCP socket)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    app_id = 'pyrdp1'
+    app = QtSingleApplication(app_id, sys.argv)
     
-    try:
-        # Connect to server and send data
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes('intersys.com.br', 'UTF-8'))
-            
-    finally:
-        sock.close()
+    if app.isRunning():
+        app.sendMessage('open:connName')
+        sys.exit(0)
+    
+    screen_rect = app.desktop().screenGeometry()
+    width, height = screen_rect.width(), screen_rect.height()
+    
+    main_window = MainWindow(width * 0.95, height * 0.90)
+    main_window.handle_message("teste")
+    main_window.show()
+    
+    app.setActivationWindow(main_window)
+    
+    sys.exit(app.exec_())
+    
--- pyrdp/ui.py
+++ pyrdp/ui.py
@@ -4,7 +4,9 @@ Created on 20/08/2013
 @author: tiago
 '''
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui, QtNetwork
+from pyrdp.rdp import Settings, RdpConnection
+from uno import unicode
 class MainWindow(QtGui.QWidget):    
     '''
     PyRdp Main Window
@@ -40,10 +42,13 @@ class MainWindow(QtGui.QWidget):
             tab = self._tabs.widget(tab_index)
             tab.close_connection()
     
-    
+    def handle_message(self, msg):
+        print('Message received %s ' % msg)
         
+        #TODO: Handle connection
     
-        
+    
+            
 class ConnectionTab(QtGui.QWidget):
     
     _conn = None
@@ -69,8 +74,79 @@ class ConnectionTab(QtGui.QWidget):
         self._conn.close()
         
         
-    
-    
-    
+class QtSingleApplication(QtGui.QApplication):
 
+    messageReceived = QtCore.pyqtSignal(unicode)
+
+    def __init__(self, appid, *argv):
+
+        super(QtSingleApplication, self).__init__(*argv)
+        self._id = appid
+        self._activationWindow = None
+        self._activateOnMessage = False
+
+        self._outSocket = QtNetwork.QLocalSocket()
+        self._outSocket.connectToServer(self._id)
+        self._isRunning = self._outSocket.waitForConnected()
+
+        if self._isRunning:
+            self._outStream = QtCore.QTextStream(self._outSocket)
+            self._outStream.setCodec('UTF-8')
+        else:
+            self._outSocket = None
+            self._outStream = None
+            self._inSocket = None
+            self._inStream = None
+            self._server = QtNetwork.QLocalServer()
+            self._server.listen(self._id)
+            self._server.newConnection.connect(self._onNewConnection)
+
+    def isRunning(self):
+        return self._isRunning
+
+    def id(self):
+        return self._id
+
+    def activationWindow(self):
+        return self._activationWindow
+
+    def setActivationWindow(self, activationWindow, activateOnMessage = True):
+        self._activationWindow = activationWindow
+        self._activateOnMessage = activateOnMessage
+        self.messageReceived.connect(self._activationWindow.handle_message)
+
+    def activateWindow(self):
+        if not self._activationWindow:
+            return
+#         self._activationWindow.setWindowState(
+#             self._activationWindow.windowState() & Qt.WindowMinimized)
+        self._activationWindow.raise_()
+        self._activationWindow.activateWindow()
+
+    def sendMessage(self, msg):
+        if not self._outStream:
+            return False
+        self._outStream << msg << '\n'
+        self._outStream.flush()
         
+        return self._outSocket.waitForBytesWritten()
+
+    def _onNewConnection(self):
+        if self._inSocket:
+            self._inSocket.readyRead.disconnect(self._onReadyRead)
+        self._inSocket = self._server.nextPendingConnection()
+        if not self._inSocket:
+            return
+        self._inStream = QtCore.QTextStream(self._inSocket)
+        self._inStream.setCodec('UTF-8')
+        self._inSocket.readyRead.connect(self._onReadyRead)
+        if self._activateOnMessage:
+            self.activateWindow()
+
+    def _onReadyRead(self):
+        while True:
+            msg = self._inStream.readLine()
+            if not msg: break
+            self.messageReceived.emit(msg)        
+        
+
